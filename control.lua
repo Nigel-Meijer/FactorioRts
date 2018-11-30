8e1a2089f89a4a397a41ea2a0bd5615ea5551de6
exa-byte <14824895+exa-byte@users.noreply.github.com>
2018-11-30 23:57:19 +0100

    improved biter command handling; added GUI

--- control.lua
+++ control.lua
@@ -1,13 +1,14 @@
 --[[============================================================================= 
-#  Author:          Snoetje
+#  Author:          Snoetje & exabyte
 #  FileName:        rtsmode.lua
 #  Description:     Rts Soft mod
-#  Version:         1.0
-#  LastChange:      2018-11-22
+#  Version:         1.1
+#  LastChange:      2018-11-30
 #  History:         
 =============================================================================--]]
 
 local event = require 'utils.event'
+local gui = require 'gui'
 
 
 local function RtsModeEnable(player)
@@ -42,62 +43,94 @@ local function on_player_deconstructed_area(event)
 	if not global.players[event.player_index].isRtsMode then return end
 	
 	if event.alt then
-		if global.players[event.player_index].selectedGroup ~= nil and #global.players[event.player_index].selectedGroup.members > 0 then
+		if global.players[event.player_index].selectedSquad ~= nil and #global.players[event.player_index].selectedSquad.members > 0 then
 			-- b_command = defines.command.attack_area
 			-- b_distraction = defines.distraction.by_damage
-		
-			-- global.selectedGroup[event.player_index].set_command({type=b_command, destination=event.area.left_top, distraction=b_distraction, radius=1})
+			-- game.print(global.players[event.player_index].selectedSquad.state)
+			-- global.players[event.player_index].selectedSquad.set_command(
+			-- {type=defines.command.wander}
+			-- -- {type=b_command, destination=event.area.left_top, distraction=b_distraction, radius=1}
+			-- )
+			-- game.print(global.players[event.player_index].selectedSquad.state)
+
 			
-			-- cache all members of old group
-			local members = global.players[event.player_index].selectedGroup.members
+			local command = {
+				type = defines.command.compound,
+				structure_type = defines.compound_command.return_last,
+				commands = {
+					{ type = defines.command.attack_area, destination = event.area.left_top, radius=4 },
+					{ type = defines.command.wander, distraction = defines.distraction.none }
+				}
+			}
+			global.players[event.player_index].selectedSquad.set_command(command)
+			global.players[event.player_index].selectedSquad.start_moving()
 			
-			-- remove old group and create a new one at target position
-			global.players[event.player_index].selectedGroup.destroy()
-			global.players[event.player_index].selectedGroup = game.surfaces[1].create_unit_group({position=event.area.left_top})
-			for _, biter in pairs(members) do
-				global.players[event.player_index].selectedGroup.add_member(biter)
-			end
+			
+			-- -- cache all members of old group
+			-- local members = global.players[event.player_index].selectedSquad.members
+			
+			-- -- remove old group and create a new one at target position
+			-- global.players[event.player_index].selectedSquad.destroy()
+			-- global.players[event.player_index].selectedSquad = game.surfaces[1].create_unit_group({position=event.area.left_top})
+			-- for _, biter in pairs(members) do
+				-- global.players[event.player_index].selectedSquad.add_member(biter)
+			-- end
 		end
 	else
 		local init_pos = {x=(event.area.left_top.x + event.area.right_bottom.x) / 2, y=(event.area.left_top.y + event.area.right_bottom.y) / 2}
+		
 		-- dont find entities filtered if area is 0 (not allowed)
 		local biters = {}
+		
 		if not (event.area.left_top.x == event.area.right_bottom.x and event.area.left_top.y == event.area.right_bottom.y) then 
+			-- area select: form new group
 			biters = game.surfaces[1].find_entities_filtered{area = event.area, type= "unit"}
+		else
+			-- click select: find nearest group in range
+			local nearestGroup = nil
+			local leastDist = 10 -- also used as max distance from click to unit group center
+			for squad, isInSet in pairs(global.players[event.player_index].squads) do
+				if not isInSet then goto continue end
+				if (not squad.valid) or #squad.members == 0 then 
+					global.players[event.player_index].squads[squad] = nil 
+					goto continue 
+				end
+				
+				local dist = math.sqrt((event.area.left_top.x - squad.position.x)^2 + (event.area.left_top.y - squad.position.y)^2)
+				if dist < leastDist then nearestGroup = squad end
+				::continue::
+			end
+			
+			global.players[event.player_index].selectedSquad = nearestGroup
+			return
 		end
 		
 		if #biters == 0 then return end
-		global.players[event.player_index].selectedGroup = game.surfaces[1].create_unit_group({position=init_pos})
+		global.players[event.player_index].selectedSquad = game.surfaces[1].create_unit_group({ position=init_pos })
+		-- add this squad to player's squads list
+		global.players[event.player_index].squads[ global.players[event.player_index].selectedSquad ] = true
+		gui.Update_Gui(game.players[ event.player_index ], "squadGui")
+		
 		
 		-- cache all groups selected biters were part of
 		local otherGroups = {}
 		for _, biter in pairs(biters) do
 			if biter.unit_group then otherGroups[biter.unit_group] = true end
-			global.players[event.player_index].selectedGroup.add_member(biter)
+			global.players[event.player_index].selectedSquad.add_member(biter)
 		end
 		
 		-- remove empty groups created by merging
-		for group, _ in pairs(otherGroups) do
-			if not (group == global.players[event.player_index].selectedGroup) then
-				game.print("group with " .. #group.members .. " members")
-				if #group.members == 0 then group.destroy() end
+		for squad, _ in pairs(otherGroups) do
+			if not (squad == global.players[event.player_index].selectedSquad) then
+				-- game.players[event.player_index].print("squad with " .. #squad.members .. " members")
+				if #squad.members == 0 then 
+					global.players[event.player_index].squads[ squad ] = nil
+					squad.destroy()
+				end
 			end
 		end
 		
-		game.print("Selected: " .. #global.players[event.player_index].selectedGroup.members)
-	end
-end
-
-local function Create_RtsButton(player)
-	if player.gui.top.RtsButton == nil then
-		local button = player.gui.top.add { name = "RtsButton", type = "sprite-button", sprite = "entity/small-biter" }
-		button.style.font = "default-bold"
-		button.style.minimal_height = 38
-		button.style.minimal_width = 38
-		button.style.top_padding = 2
-		button.style.left_padding = 4
-		button.style.right_padding = 4
-		button.style.bottom_padding = 2
+		game.print("Selected: " .. #global.players[event.player_index].selectedSquad.members .. " biters")
 	end
 end
 
@@ -108,13 +141,21 @@ local function OnPlayerCreated(event)
 	
 	global.players[event.player_index] = {}
 
-	global.players[event.player_index].selectedGroup	= nil
+	global.players[event.player_index].selectedSquad	= nil
 	global.players[event.player_index].nameBackup 		= ""
 	global.players[event.player_index].colorBackup 		= nil
 	global.players[event.player_index].isRtsMode 			= false
+	global.players[event.player_index].squads				= {}
+	
+	-- init upgrades
+	global.players[event.player_index].upgrades 			= {}
+	global.players[event.player_index].upgrades.numOfSquads 	= { name = "Max number of squads", value = 1 }
+	global.players[event.player_index].upgrades.squadSize 		= { name = "Maximum squad size", value = 10 }
+	
+	
 	
 	local player = game.players[event.player_index]	
-	Create_RtsButton(player)
+	gui.Create_RtsButton(player)
 	
 	for name, recipe in pairs(player.force.recipes) do 
 		recipe.enabled = true 
@@ -130,8 +171,9 @@ end
 
 local function on_gui_click(event)
 	local player = game.players[event.player_index]
-	
+
 	local name = event.element.name
+
 	if name == "RtsButton" then		
 		if IsRtsModeEnabled(player) == false then
 			RtsModeEnable(player)
@@ -151,6 +193,47 @@ local function on_gui_click(event)
 			RestorePlayer(player)
 			
 		end
+	
+	elseif name == "UpgradeMenuButton" then
+		gui.Toggle_Gui(player, "upgradeGui")
+		gui.Toggle_Gui(player, "squadGui")
+	
+	
+	elseif string.find(name, "^squad_previewButton") then
+		local squadNum = tonumber(string.sub(name, -1))
+		local foundSquad = nil
+		local i = 0
+		
+		for squad, isInSet in pairs(global.players[player.index].squads) do
+			if not isInSet then goto continue end
+			if (not squad.valid) --[[or #squad.members == 0]] then 
+				global.players[player.index].squads[squad] = nil 
+				goto continue
+			end
+			if i == squadNum then 
+				foundSquad = squad
+				break
+			end
+			i = i + 1
+			::continue::
+		end
+		game.players[event.player_index].teleport(foundSquad.position)
+		global.players[player.index].selectedSquad = foundSquad
+	end
+end
+
+local function on_tick(event)
+	if global.players[1].isRtsMode then
+		if event.tick % 60 == 0 then
+			gui.Update_Gui(game.players[1], "squadGui")
+		end
+		if event.tick % 1 == 0 then
+			for _, player in pairs(game.players) do
+				if global.players[player.index].isRtsMode then
+					gui.Update_SquadGui_Tick(player)
+				end
+			end
+		end
 	end
 end
 
@@ -158,3 +241,6 @@ event.add(defines.events.on_marked_for_deconstruction, on_marked_for_deconstruct
 event.add(defines.events.on_player_deconstructed_area, on_player_deconstructed_area)
 event.add(defines.events.on_player_created, OnPlayerCreated)
 event.add(defines.events.on_gui_click, on_gui_click)
+
+
+event.add(defines.events.on_tick, on_tick)
---
+++ gui.lua
@@ -0,0 +1,133 @@
+local Gui = {}
+
+function Gui.Create_Gui(player, guiName)
+	if guiName == "upgradeGui" then
+		local upgradeGui = player.gui.top.rtsGui.add{ name = "upgradeGui", type = "frame", caption = "Upgrades", direction = "vertical" }
+		local upgradesTable = upgradeGui.add{ name = "upgradesTable", type = "table", column_count = 2 }
+		for k,v in pairs(global.players[player.index].upgrades) do
+			upgradesTable.add{ name = v.name, type = "label", caption = tostring(v.name) }
+			upgradesTable.add{ name = v.name .. "_value", type = "label", caption = tostring(v.value) }
+		end
+		
+	elseif guiName == "squadGui" then
+		local squadGui = player.gui.top.rtsGui.add{ name = "squadGui", type = "frame", caption = "Squads", direction = "vertical" }
+		local squadTable = squadGui.add{ name = "squadTable", type = "table", column_count = 3 }
+		-- add table head
+		local tableHeadColor = { r=1, g=.5, b=.5, a=1 }
+		squadTable.add{ name = "squad_id", type = "label", caption = "squad id" }.style.font_color = tableHeadColor
+		squadTable.add{ name = "squad_numMembers", type = "label", caption = "squad size" }.style.font_color = tableHeadColor
+		squadTable.add{ name = "squad_preview", type = "label", caption = "preview" }.style.font_color = tableHeadColor
+		
+		-- fill table with squads
+		local i = 0
+		for squad, isInSet in pairs(global.players[player.index].squads) do
+
+			if not isInSet then goto continue end
+			if (not squad.valid) then 
+				global.players[player.index].squads[squad] = nil 
+				goto continue
+			end
+			if #squad.members == 0 then goto continue end
+			local l1 = squadTable.add{ name = "squad_id_" .. i, type = "label", caption = "squad " .. i }
+			local l2 = squadTable.add{ name = "squad_numMembers_" .. i, type = "label", caption = "" .. #squad.members }
+			local fontColor = { r=1, g=1, b=1, a=1 }
+			if squad == global.players[player.index].selectedSquad then
+				fontColor = { r=.5, g=1, b=.5, a=1 }
+			end
+			l1.style.font_color = fontColor
+			l2.style.font_color = fontColor
+			local button = squadTable.add{ name = "squad_previewButton_" .. i, type = "button" }
+			local preview = button.add{ name = "squad_preview_" .. i, type = "camera", position = squad.position, zoom = 0.25}
+			button.style.minimal_height = 112
+			button.style.minimal_width = 112
+			preview.style.minimal_height = 100
+			preview.style.minimal_width = 100
+			preview.ignored_by_interaction = true
+			-- game.print("i: "..i)
+			i = i + 1
+			::continue::
+		end
+	end
+end
+
+function Gui.Update_Gui(player, guiName)
+	if not player.gui.top.rtsGui[guiName] then return false end
+	
+	if guiName == "upgradeGui" then
+	
+		player.gui.top.rtsGui[guiName].destroy()
+		Gui.Create_Gui(player, guiName)
+		
+	elseif guiName == "squadGui" then
+	
+		player.gui.top.rtsGui[guiName].destroy()
+		Gui.Create_Gui(player, guiName)
+		
+	end
+end
+
+function Gui.Update_SquadGui_Tick(player)
+	local i = 0
+	for squad, isInSet in pairs(global.players[player.index].squads) do
+		if not isInSet then goto continue end
+		if (not squad.valid) then 
+			global.players[player.index].squads[squad] = nil 
+			Gui.Update_Gui(player, "squadGui")
+			return
+		end
+		if #squad.members == 0 then Gui.Update_Gui(player, "squadGui"); return end
+		
+		if 	not player.gui.top.rtsGui.squadGui or 
+			not player.gui.top.rtsGui.squadGui.squadTable["squad_previewButton_" .. i] then 
+			goto continue 
+		end
+		local fontColor = { r=1, g=1, b=1, a=1 }
+		if squad == global.players[player.index].selectedSquad then
+			fontColor = { r=.5, g=1, b=.5, a=1 }
+		end
+		player.gui.top.rtsGui.squadGui.squadTable["squad_id_" .. i].style.font_color = fontColor
+		player.gui.top.rtsGui.squadGui.squadTable["squad_numMembers_" .. i].style.font_color = fontColor
+		player.gui.top.rtsGui.squadGui.squadTable["squad_previewButton_" .. i]["squad_preview_" .. i].position = squad.position
+		
+		i = i + 1
+		::continue::
+	end
+end
+
+function Gui.Toggle_Gui(player, guiName)
+	if player.gui.top.rtsGui[guiName] ~= nil then
+		player.gui.top.rtsGui[guiName].destroy()
+	else
+		Gui.Create_Gui(player, guiName)
+	end
+end
+
+function Gui.Create_RtsButton(player)
+	if not player.gui.top.rtsGui then
+		local rtsGui = player.gui.top.add{ name = "rtsGui", type = "flow", direction = "vertical" }
+		
+		local topRow = rtsGui.add{ name = "topRow", type = "flow", direction = "horizontal" }
+		
+		local button = topRow.add { name = "RtsButton", type = "sprite-button", sprite = "entity/small-biter" }
+		button.style.font = "default-bold"
+		button.style.minimal_height = 38
+		button.style.minimal_width = 38
+		button.style.top_padding = 2
+		button.style.left_padding = 4
+		button.style.right_padding = 4
+		button.style.bottom_padding = 2
+		
+		local button = topRow.add { name = "UpgradeMenuButton", type = "sprite-button", sprite = "item-group/fluids", tooltip = "Upgrades" }
+		button.style.font = "default-bold"
+		button.style.minimal_height = 38
+		button.style.minimal_width = 38
+		button.style.top_padding = 2
+		button.style.left_padding = 4
+		button.style.right_padding = 4
+		button.style.bottom_padding = 2
+		
+		Gui.Create_Gui(player, "upgradeGui")
+	end
+end
+
+return Gui
