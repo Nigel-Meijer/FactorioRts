6a621edff4a6f4cf7db488d090f3af77dec49312
Nigel <nigel_meijer@hotmail.com>
2018-12-08 12:41:05 +0100

    Isolated Squadmove/Attack/Select() functions

--- control.lua
+++ control.lua
@@ -39,100 +39,115 @@ local function RestorePlayer(player)
 	player.color = global.players[player.index].nameBackup
 end
 
+
+local function SquadAttack()
+	if global.players[player_index].selectedSquad ~= nil and #global.players[player_index].selectedSquad.members > 0 then
+		local command = {
+			type = defines.command.compound,
+			structure_type = defines.compound_command.return_last,
+			commands = {
+				{ type = defines.command.attack_area, destination = area.left_top, radius=4 },
+				{ type = defines.command.wander, distraction = defines.distraction.none }
+			}
+		}
+		global.players[player_index].selectedSquad.set_command(command)
+		global.players[player_index].selectedSquad.start_moving()
+	end
+end
+
+local function SquadMove(area,player_index)
+	if global.players[player_index].selectedSquad ~= nil and #global.players[player_index].selectedSquad.members > 0 then
+		local command = {
+			type = defines.command.compound,
+			structure_type = defines.compound_command.return_last,
+			commands = {
+				{ type = defines.command.go_to_location, distraction = defines.distraction.none, destination = area.left_top,  radius=4 },
+				{ type = defines.command.wander, 		 distraction = defines.distraction.none }
+			}
+		}
+		global.players[player_index].selectedSquad.set_command(command)
+		global.players[player_index].selectedSquad.start_moving()
+	end
+end
+
+
+local function SquadSelect(area,player_index)
+	local init_pos = {x=(area.left_top.x + area.right_bottom.x) / 2, y=(area.left_top.y + area.right_bottom.y) / 2}
+		
+	-- dont find entities filtered if area is 0 (not allowed)
+	local biters = {}
+	
+	if not (area.left_top.x == area.right_bottom.x and area.left_top.y == area.right_bottom.y) then 
+		-- area select: form new group
+		biters = game.surfaces[1].find_entities_filtered{area = area, type= "unit"}
+	else
+		--[[ TODO -- Repurpose this code
+		-- click select: find nearest group in range
+		local nearestGroup = nil
+		local leastDist = 10 -- also used as max distance from click to unit group center
+		for squad, isInSet in pairs(global.players[event.player_index].squads) do
+			if not isInSet then goto continue end
+			if (not squad.valid) or #squad.members == 0 then 
+				global.players[event.player_index].squads[squad] = nil 
+				goto continue 
+			end
+			
+			local dist = math.sqrt((event.area.left_top.x - squad.position.x)^2 + (event.area.left_top.y - squad.position.y)^2)
+			if dist < leastDist then nearestGroup = squad end
+			::continue::
+		end
+		
+		global.players[event.player_index].selectedSquad = nearestGroup
+		return
+		]]--
+	end
+	
+	if #biters == 0 then return end
+	-- find a suitable non spawner-colliding position for the unit group to form
+	local non_col_pos = game.surfaces[1].find_non_colliding_position("rocket-silo", init_pos, 0, 1)
+	global.players[player_index].selectedSquad = game.surfaces[1].create_unit_group({ position=non_col_pos })
+	-- add this squad to player's squads list
+	global.players[player_index].squads[global.players[player_index].selectedSquad] = true
+	gui.Update_Gui(game.players[player_index], "squadGui")
+	
+	
+	-- cache all groups selected biters were part of
+	local otherGroups = {}
+	for _, biter in pairs(biters) do
+		if biter.unit_group then otherGroups[biter.unit_group] = true end
+		global.players[player_index].selectedSquad.add_member(biter)
+	end
+	
+	-- remove empty groups created by merging
+	for squad, _ in pairs(otherGroups) do
+		if not (squad == global.players[player_index].selectedSquad) then
+			-- game.players[event.player_index].print("squad with " .. #squad.members .. " members")
+			if #squad.members == 0 then 
+				global.players[player_index].squads[ squad ] = nil
+				squad.destroy()
+			end
+		end
+	end
+	
+	game.print("Selected: " .. #global.players[player_index].selectedSquad.members .. " biters")
+
+end
+
+
+
+
+
+
+
+
+
 local function on_player_deconstructed_area(event)
 	if not global.players[event.player_index].isRtsMode then return end
 	
 	if event.alt then
-		if global.players[event.player_index].selectedSquad ~= nil and #global.players[event.player_index].selectedSquad.members > 0 then
-			-- b_command = defines.command.attack_area
-			-- b_distraction = defines.distraction.by_damage
-			-- game.print(global.players[event.player_index].selectedSquad.state)
-			-- global.players[event.player_index].selectedSquad.set_command(
-			-- {type=defines.command.wander}
-			-- -- {type=b_command, destination=event.area.left_top, distraction=b_distraction, radius=1}
-			-- )
-			-- game.print(global.players[event.player_index].selectedSquad.state)
-
-			
-			local command = {
-				type = defines.command.compound,
-				structure_type = defines.compound_command.return_last,
-				commands = {
-					{ type = defines.command.attack_area, destination = event.area.left_top, radius=4 },
-					{ type = defines.command.wander, distraction = defines.distraction.none }
-				}
-			}
-			global.players[event.player_index].selectedSquad.set_command(command)
-			global.players[event.player_index].selectedSquad.start_moving()
-			
-			
-			-- -- cache all members of old group
-			-- local members = global.players[event.player_index].selectedSquad.members
-			
-			-- -- remove old group and create a new one at target position
-			-- global.players[event.player_index].selectedSquad.destroy()
-			-- global.players[event.player_index].selectedSquad = game.surfaces[1].create_unit_group({position=event.area.left_top})
-			-- for _, biter in pairs(members) do
-				-- global.players[event.player_index].selectedSquad.add_member(biter)
-			-- end
-		end
+		SquadMove(event.area, event.player_index)
 	else
-		local init_pos = {x=(event.area.left_top.x + event.area.right_bottom.x) / 2, y=(event.area.left_top.y + event.area.right_bottom.y) / 2}
-		
-		-- dont find entities filtered if area is 0 (not allowed)
-		local biters = {}
-		
-		if not (event.area.left_top.x == event.area.right_bottom.x and event.area.left_top.y == event.area.right_bottom.y) then 
-			-- area select: form new group
-			biters = game.surfaces[1].find_entities_filtered{area = event.area, type= "unit"}
-		else
-			-- click select: find nearest group in range
-			local nearestGroup = nil
-			local leastDist = 10 -- also used as max distance from click to unit group center
-			for squad, isInSet in pairs(global.players[event.player_index].squads) do
-				if not isInSet then goto continue end
-				if (not squad.valid) or #squad.members == 0 then 
-					global.players[event.player_index].squads[squad] = nil 
-					goto continue 
-				end
-				
-				local dist = math.sqrt((event.area.left_top.x - squad.position.x)^2 + (event.area.left_top.y - squad.position.y)^2)
-				if dist < leastDist then nearestGroup = squad end
-				::continue::
-			end
-			
-			global.players[event.player_index].selectedSquad = nearestGroup
-			return
-		end
-		
-		if #biters == 0 then return end
-		-- find a suitable non spawner-colliding position for the unit group to form
-		local non_col_pos = game.surfaces[1].find_non_colliding_position("rocket-silo", init_pos, 0, 1)
-		global.players[event.player_index].selectedSquad = game.surfaces[1].create_unit_group({ position=non_col_pos })
-		-- add this squad to player's squads list
-		global.players[event.player_index].squads[ global.players[event.player_index].selectedSquad ] = true
-		gui.Update_Gui(game.players[ event.player_index ], "squadGui")
-		
-		
-		-- cache all groups selected biters were part of
-		local otherGroups = {}
-		for _, biter in pairs(biters) do
-			if biter.unit_group then otherGroups[biter.unit_group] = true end
-			global.players[event.player_index].selectedSquad.add_member(biter)
-		end
-		
-		-- remove empty groups created by merging
-		for squad, _ in pairs(otherGroups) do
-			if not (squad == global.players[event.player_index].selectedSquad) then
-				-- game.players[event.player_index].print("squad with " .. #squad.members .. " members")
-				if #squad.members == 0 then 
-					global.players[event.player_index].squads[ squad ] = nil
-					squad.destroy()
-				end
-			end
-		end
-		
-		game.print("Selected: " .. #global.players[event.player_index].selectedSquad.members .. " biters")
+		SquadSelect(event.area,event.player_index)
 	end
 end
 
